6de613f0b24e76fb7ec8bbb37d416ad6cb560c68
Florent EXBRAYAT <florent.exbrayat@amadeus.com>
2017-06-21 10:03:30 +0200

    add more target clean

--- src/app/main.aot.ts
+++ src/app/main.aot.ts
@@ -1,4 +1,6 @@
 import { platformBrowser } from '@angular/platform-browser';
+import {enableProdMode} from '@angular/core';
 import { MainModuleNgFactory } from './modules/main.module.ngfactory';
 
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(MainModuleNgFactory);
